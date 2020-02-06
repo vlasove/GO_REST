b9b6acf31676668c0bb03776d8ac835bc7873f25
evgen <evgeny_vlasov@yahoo.com>
2020-02-06 11:56:28 +0000

    model done

--- model.go
+++ model.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"errors"
+	"fmt"
 )
 
 //Book ...
@@ -14,25 +14,44 @@ type Book struct {
 
 //getBook ...
 func (b *Book) getBook(db *sql.DB) error {
-	return errors.New("Not yet")
+	err := db.QueryRow("SELECT * FROM books WHERE id=$1", b.ID).Scan(&b.ID, &b.Name, &b.Price)
+	return err
 }
 
 //updateBook
 func (b *Book) updateBook(db *sql.DB) error {
-	return errors.New("Not yet")
+	_, err := db.Exec("UPDATE books SET name = $1, price = $2  WHERE id = $3", b.Name, b.Price, b.ID)
+	return err
 }
 
 //createBook
 func (b *Book) createBook(db *sql.DB) error {
-	return errors.New("Not yet")
+	_, err := db.Exec("INSERT INTO books (name, price) VALUES ($1, $2)", b.Name, b.Price)
+	return err
 }
 
 //deleteBook
 func (b *Book) deleteBook(db *sql.DB) error {
-	return errors.New("Not yet")
+	_, err := db.Exec("DELETE FROM books WHERE id = $1", b.ID)
+	return err
 }
 
 //getBooks ...
 func getBooks(db *sql.DB) ([]Book, error) {
-	return nil, errors.New("Not yet")
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		return nil, err
+	}
+
+	books := make([]Book, 0)
+	for rows.Next() {
+		var b Book
+		if err := rows.Scan(&b.ID, &b.Name, &b.Price); err != nil {
+			fmt.Println(err)
+			continue
+		}
+		books = append(books, b)
+	}
+
+	return books, nil
 }
